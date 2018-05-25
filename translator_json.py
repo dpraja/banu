2a3c7078e32b0fe754a4aff3c53c198f9a80a082
infocuittesting <38007487+infocuittesting@users.noreply.github.com>
2018-05-25 15:53:58 +0530

    Update translator_json.py

--- translator_json.py
+++ translator_json.py
@@ -4,16 +4,13 @@ import goslate
 from gtts import gTTS
 from playsound import playsound
 import os
-app = Flask(__name__)
-@app.route("/helloworld",methods=['POST'])
-def audi():
+def mytranslator(request):
     text = request.json['text']
     gs = goslate.Goslate()
     translatedText = gs.translate(text.encode(encoding='UTF-8'),'ar')
     print(translatedText)
     return(json.dumps({'Status':'sucess','Message': translatedText }))
-if __name__ == "__main__":
-    app.run(host="192.168.1.4",port=5000)
+
 
 
 
