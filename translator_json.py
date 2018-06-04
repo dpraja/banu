750767f9ca875f547eadc82ceebc04bcb09d0a3b
dpraja <raja.pushpanathan@gmail.com>
2018-06-04 16:13:59 +0530

    Update translator_json.py

--- translator_json.py
+++ translator_json.py
@@ -8,7 +8,7 @@ def mytranslator(request):
     language = 'ta'
     myobj = gTTS(text=mytext, lang=language, slow=False)
     myobj.save("welcome.mp3")
-    playsound("C:/Users/dell/AppData/Local/Programs/Python/Python35/welcome.mp3")
+    playsound("https://s3.amazonaws.com/infocuithtml/welcome.mp3")
     print("printing")
     return(json.dumps({'Status': 'Success','Message': 'Computer Think! You Say This '},indent=4))
 
