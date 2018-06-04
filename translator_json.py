00b6ff8ebe6ed65697e46e888a9bb5152702d6e7
dpraja <raja.pushpanathan@gmail.com>
2018-06-04 15:12:55 +0530

    Update translator_json.py

--- translator_json.py
+++ translator_json.py
@@ -1,15 +1,17 @@
 from flask import Flask,request
 import json
-import goslate
 from gtts import gTTS
 from playsound import playsound
 import os
-def mytranslator(request):
-    text = request.json['text']
-    gs = goslate.Goslate()
-    translatedText = gs.translate(text.encode(encoding='UTF-8'),'ar')
-    print(translatedText)
-    return(json.dumps({'Status':'sucess','Message': translatedText }))
+def audi(request):
+    mytext = request.json['mytext']
+    language = 'ta'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("welcome.mp3")
+    playsound("C:/Users/dell/AppData/Local/Programs/Python/Python35/welcome.mp3")
+    print("printing")
+    return(json.dumps({'Status': 'Success','Message': 'Computer Think! You Say This '},indent=4))
+
 
 
 
