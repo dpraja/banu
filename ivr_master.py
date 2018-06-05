e3baa3360568be5c4e1089cae9aa734ff9b444c6
dpraja <raja.pushpanathan@gmail.com>
2018-06-05 12:11:46 +0530

    Update ivr_master.py

--- ivr_master.py
+++ ivr_master.py
@@ -6,6 +6,7 @@ import json
 from flask import Flask,request, jsonify
 from flask_cors import CORS
 from translator_json import mytranslator
+from audiomp3url import audiomp3url
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +18,9 @@ def hello():
 def texttospeech():
    return mytranslator(request)
 
-
+@app.route("/Audiomp3",methods=['GET'])
+def index():
+   return audiomp3url()
 
 
 if __name__ == "__main__":
